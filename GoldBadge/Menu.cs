92d52e917905ce0cc7d82361e0bac6018c698a3c
garenbragg <55403044+garenbragg@users.noreply.github.com>
2020-04-09 20:24:15 -0400

    fixed missing UI

---
+++ GoldBadge/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe
+{
+    public enum Course { Breakfast, Lunch, Dinner, Appetizer, Drink, Dessert}
+    public class Menu
+    {
+        public decimal Price { get; set; }
+        public int ItemNumber { get; set; }
+        public string Name { get; set; }
+        public Course ItemType { get; set; }
+
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public Menu() { }
+      
+        public Menu(decimal price, int itemnumber, string name, Course itemtype, string ingredients)
+        {
+            Price = price;
+            ItemNumber = itemnumber;
+            Name = name;
+            ItemType = itemtype;
+            Ingredients = ingredients;
+        }
+    }
+}
